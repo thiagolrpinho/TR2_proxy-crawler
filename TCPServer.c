9b53496ab95127526910b530c7cc631b2b81bfb0
Thiago <thiagobarros@cjr.org.br>
2019-11-04 20:38:20 -0300

    Criando o TCPServer seguindo o tutorial

---
+++ TCPServer.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <netinet/in.h>
+
+int main() {
+
+  char server_message[256] = "Hello i am the server";
+
+  //Cria Socket
+  int server_socket;
+  server_socket = socket(AF_INET,SOCK_STREAM,0);
+
+  //Define o endereço do Socket
+  char server_ip[15];
+  struct sockaddr_in server_address;
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(8228);
+  server_address.sin_addr.s_addr = server_ip;
+
+  //Associa o IP e a porta ao socket
+  bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+
+  listen(server_socket, 0);
+
+  int client_socket;
+  client_socket = accept(server_socket, NULL, NULL);
+
+  //Envia a mensagem
+  send(client_socket, server_message, sizeof(server_message), 0);
+
+  //Encerra conexão
+  close(server_socket);
+
+  return 0;
+}
