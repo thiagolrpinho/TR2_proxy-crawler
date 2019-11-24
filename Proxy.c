48b250e8415b847600f6a59d7beaff7dfbb5ad46
Thiago <thiagobarros@cjr.org.br>
2019-11-24 18:52:07 -0300

    Criando primeira regex para extração do método connect do cabeçalho http

--- Proxy.c
+++ Proxy.c
@@ -7,6 +7,8 @@
 
 #include <netinet/in.h>
 
+#include <regex.h>
+
 int main() {
 
   char http_header[2048] = "HTTP/1.1 200 OK\r\n\n";
@@ -41,23 +43,65 @@ int main() {
 
   listen(server_socket, 3);
 
-
+  char method[7];
   char request[1024];
   int client_socket;
+
+  //Cria e Compila a Regex que vai ser utilizada para minerar os dados do request/response
+  regex_t connect_regex;
+  int connect_reti;
+  char msgbuf[100];
+  connect_reti = regcomp(&connect_regex, "CONNECT", 0);
+  if (connect_reti) {
+      printf("Could not compile regex\n");
+      exit(1);
+  }
+
+
+
+
   //Deixa o servidor escutando
   while(1) {
+    //Limpa a variavel que guarda o método do request/response
+    memset(method, 0, sizeof method);
+    //
     client_socket = accept(server_socket, NULL, NULL);
     //Recebe o request
     recv(client_socket, request, sizeof(request), 0);
+    //Printa o request no terminal
+    printf("%s \n ----------------------------------------\n",request);
+
+    /* Executa expressão regular de método*/
+    connect_reti = regexec(&connect_regex, request, 0, NULL, 0);
+    if (!connect_reti) {
+        printf("Método: Connect\n");
+
+    }
+    else if (connect_reti == REG_NOMATCH) {
+        printf("No match");
+    }
+    else if (connect_reti == REG_NOMATCH) {
+        printf("No match");
+    }
+    else if (connect_reti == REG_NOMATCH) {
+        printf("No match");
+    }
+    else {
+        regerror(connect_reti, &connect_regex, msgbuf, sizeof(msgbuf));
+        printf("Regex match failed: %s\n", msgbuf);
+        exit(1);
+    }
+
+
     //Envia a mensagem
     send(client_socket, http_header, sizeof(http_header), 0);
 
-    printf("%s \n ----------------------------------------\n",request);
+    /* Free memory allocated to the pattern buffer by regcomp() */
     //Encerra conexão
     close(client_socket);
 
   }
 
-
+  regfree(&connect_regex);
   return 0;
 }
