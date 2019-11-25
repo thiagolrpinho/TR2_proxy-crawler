70b45ac5e604ccaa2a8d44406aaee6e0da0b57a4
Thiago <thiagobarros@cjr.org.br>
2019-11-24 22:56:58 -0300

    Modularizando criação de socket cliente e servidor, sucesso ao tentar fazer conexões seguidas atravéz do proxy

--- Proxy.c
+++ Proxy.c
@@ -10,6 +10,38 @@
 
 #include <regex.h>
 
+int create_client_socket(char* address,int port){
+  //Cria Socket
+  int client_socket = socket(AF_INET,SOCK_STREAM,0);
+  struct sockaddr_in client_address;
+  //Especifica o endereço e a porta para o Client Socket
+  client_address.sin_family = AF_INET;
+  client_address.sin_port = htons(port);
+  client_address.sin_addr.s_addr = inet_addr(address);
+  //Connecta/Verifica conexão
+  int connection_status = connect(client_socket, (struct sockaddr *) &client_address, sizeof(client_address));
+  if(connection_status == -1){
+    printf("Ocorreu um erro ao tentar estabalecer conexão com o socket");
+  }
+  return client_socket;
+}
+
+int create_server_socket(char* address,int port){
+  //Cria Socket
+  int server_socket = socket(AF_INET,SOCK_STREAM,0);
+  struct sockaddr_in server_address;
+  //Especifica o endereço e a porta para o Server Socket
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(port);
+  server_address.sin_addr.s_addr = inet_addr(address);
+  //Associa o IP e a porta ao Server Socket
+  bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+  //Fica escutando o Server Socket
+  listen(server_socket, 3);
+
+  return server_socket;
+}
+
 int main() {
 
   char http_header[2048] = "HTTP/1.1 200 OK\r\n\n";
@@ -28,38 +60,6 @@ int main() {
   //Concatena a resposta no http_header
   strcat(http_header, response_data);
 
-  //Cria Socket de Envio
-  int sender_socket;
-  sender_socket = socket(AF_INET,SOCK_STREAM,0);
-  struct sockaddr_in server_sender_address;
-
-  //Especifica um endereço para o Socket de Envio
-  int server_ip = inet_addr("127.0.0.1");
-  server_sender_address.sin_family = AF_INET;
-  server_sender_address.sin_port = htons(8002);
-  server_sender_address.sin_addr.s_addr = server_ip;
-
-  //Cria Socket Receptor
-  int server_socket;
-  server_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-  //Define o endereço do Socket Receptor
-  server_ip = INADDR_ANY;
-  struct sockaddr_in server_address;
-  server_address.sin_family = AF_INET;
-  server_address.sin_port = htons(8001);
-  server_address.sin_addr.s_addr = server_ip;
-
-  //Associa o IP e a porta ao Socket Receptor
-  bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
-
-  //Deixa o programa escutando o Socket Receptor
-  listen(server_socket, 3);
-
-  char method[7];
-  char request[1024];
-  int client_socket;
-
   //Cria e Compila a Regex que vai ser utilizada para minerar os dados do request/response
   regex_t connect_regex;
   int connect_reti;
@@ -70,10 +70,12 @@ int main() {
       exit(1);
   }
 
+  //Cria socket de Recepção e deixa listening
+  int server_socket = create_server_socket("127.0.0.1",8001);
 
-
-
-  //Deixa o servidor escutando
+  char method[7];
+  char request[1024];
+  int client_socket;
   while(1) {
     //Limpa a variavel que guarda o método do request/response
     memset(method, 0, sizeof method);
@@ -88,16 +90,9 @@ int main() {
     connect_reti = regexec(&connect_regex, request, 0, NULL, 0);
     if (!connect_reti) {
       printf("Método: Connect\n");
+      //Cria o socket cliente como Socket de Envio, para fazer a requisição ao servidor de destino
+      int sender_socket = create_client_socket("127.0.0.1", 8002);
 
-      //Especifica um endereço para o Socket de Envio, Comentado por motivo de teste
-      /*server_ip = inet_addr("127.0.0.1");
-      server_sender_address.sin_addr.s_addr = server_ip;*/
-
-      //Connecta/Verifica conexão
-      int connection_status = connect(sender_socket, (struct sockaddr *) &server_sender_address, sizeof(server_sender_address));
-      if(connection_status == -1){
-        printf("Ocorreu um erro ao tentar estabalecer conexão com o socket");
-      }
       //Teste//Envia a requisição ao destino,pelo Socket de Envio, e pega a resposta
       send(sender_socket, request, sizeof(request), 0);
       recv(sender_socket, &request, sizeof(request), 0);
